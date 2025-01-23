a2b48967fb10319ec9f858ce0da1c78235abbdae
Marno van der Maas <mvdmaas+git@lowrisc.org>
2025-01-23 11:24:03 +0000

    Nix formatting

--- nix/lowrisc_sail.nix
+++ nix/lowrisc_sail.nix
@@ -1,13 +1,11 @@
 # Copyright lowRISC contributors.
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
-
 # Override the upstream sail package definition to use the lowRISC fork
 {
   pkgs,
   src,
 }:
-
 pkgs.ocamlPackages.sail.overrideAttrs (prev: {
   pname = "lowrisc_sail";
   inherit src;
@@ -15,9 +13,11 @@ pkgs.ocamlPackages.sail.overrideAttrs (prev: {
   # The lowRISC fork is older than upstream, and requires additional dependencies
   # from those specified upsteam to build. Add them here.
   propagatedBuildInputs =
-    prev.propagatedBuildInputs ++ (with pkgs.ocamlPackages; [
+    prev.propagatedBuildInputs
+    ++ (with pkgs.ocamlPackages; [
       menhirLib
-    ]) ++ (with pkgs; [
+    ])
+    ++ (with pkgs; [
       z3
     ]);
 })
